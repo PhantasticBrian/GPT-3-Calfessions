580c00a62cf80aef5e576175c7e15d66f0977e39
Brian Phan <b.phan@berkeley.edu>
2021-11-20 22:05:27 -0800

    Improvements

--- ocr.py
+++ ocr.py
@@ -11,17 +11,17 @@ def detect_text_uri(uri):
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print("Texts:")
+    # print("Texts:")
 
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        # print('\n"{}"'.format(text.description))
 
         vertices = [
             "({},{})".format(vertex.x, vertex.y)
             for vertex in text.bounding_poly.vertices
         ]
 
-        print("bounds: {}".format(",".join(vertices)))
+        # print("bounds: {}".format(",".join(vertices)))
     if response.error.message:
         raise Exception(
             "{}\nFor more info on error messages, check: "
@@ -32,8 +32,10 @@ def detect_text_uri(uri):
 
 def get_text(uri):
     text = detect_text_uri(uri)
-
-    if text[0:3] == ["CALFESSION", "CAL", "CONFESSION"]:
+    len_text = len(text)
+    if len_text < 3:
+        return None
+    elif text[0:3] == ["CALFESSION", "CAL", "CONFESSION"]:
         text = text[3:]
     elif text[-1] == "CALFESSION":
         text = text[:-1]
@@ -48,13 +50,22 @@ posts_uris = [item["value"] for item in json.loads(posts_content)]
 
 confessions = []
 
-for uri in posts_uris:
-    text = get_text(uri)
-    confessions.append(" ".join(text))
 
-confessions_file = open("confessions.json", "w")
-confessions_file.write(json.dumps(confessions))
-confessions_file.close()
+def write_confessions():
+    confessions_file = open("confessions.json", "w")
+    confessions_file.write(json.dumps(confessions))
+    confessions_file.close()
+
+
+num_posts = len(posts_uris)
+for index, uri in enumerate(posts_uris):
+    print(index + 1, "/", num_posts, ":", uri)
+    text = get_text(uri)
+    if text:
+        confessions.append(" ".join(text))
+    if index % 10 == 0:
+        write_confessions()
+write_confessions()
 
 """
 /** Jank browser console scraper code: */
